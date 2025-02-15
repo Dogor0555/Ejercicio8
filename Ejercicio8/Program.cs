0e28f8085c1eb7c246768f82d3471da7c22bb478
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 05:58:02 -0600

    Ejercicio8

--- Ejercicio8/Program.cs
+++ Ejercicio8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+Console.Write("Ingrese una fecha en formato dd/mm/yyyy: ");
+string fechaStr = Console.ReadLine();
+
+DateTime fecha = DateTime.ParseExact(fechaStr, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+Console.WriteLine("Fecha en formato largo: " + fecha.ToString("dddd, dd MMMM yyyy"));
